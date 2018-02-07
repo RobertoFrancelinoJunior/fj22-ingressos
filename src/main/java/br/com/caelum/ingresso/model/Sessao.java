ec4085309e52d78a3fc7a13055ed4230acc6ef84
roberto junio <francelinoj@gmail.com>
2018-02-07 20:30:55 -0200

    adicionando comportamento de sessao

---
+++ src/main/java/br/com/caelum/ingresso/model/Sessao.java
@@ -0,0 +1,75 @@
+package br.com.caelum.ingresso.model;
+
+import java.time.LocalTime;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+@Entity
+public class Sessao {
+	
+	@Id
+	@GeneratedValue
+	private Integer id;
+	
+	private LocalTime horario;
+	
+	@ManyToOne
+	private Sala sala;
+	
+	@ManyToOne
+	private Filme filme;
+
+	public Sessao()
+	{
+	}
+
+	public Sessao(LocalTime horario, Filme filme, Sala sala)
+	{
+		this.horario = horario;
+		this.filme = filme;
+		this.sala = sala;
+	}
+	
+	public void setId(Integer id)
+	{
+		this.id = id;
+	}
+	
+	public Integer getId()
+	{
+		return this.id;
+	}
+	
+	public void setSala(Sala sala)
+	{
+		this.sala = sala;
+	}
+	
+	public Sala getSala()
+	{
+		return this.sala;
+	}
+	
+	public void setFilme(Filme filme)
+	{
+		this.filme = filme;
+	}
+	
+	public Filme getFilme()
+	{
+		return this.filme;
+	}
+	
+	public void setHorario(LocalTime horario)
+	{
+		this.horario = horario;
+	}
+	
+	public LocalTime getHorario()
+	{
+		return this.horario;
+	}
+}
